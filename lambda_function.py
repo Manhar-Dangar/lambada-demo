8fe74babba03c8ada88e7e036b173c8a5e2237f8
ManharDangar <manhar.dangar.sa@gmail.com>
2022-05-20 15:41:50 +0530

    add new one

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
+    d = {'col1': [1,2], 'col2': [3,4], 'col3': [2,1]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Hello version 1')
+    print('Hello version 2')
     
